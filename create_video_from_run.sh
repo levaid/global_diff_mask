fddf8ad38f7e5748427bd09d0d46e0217c6ee83c
levaid <levai753@gmail.com>
2020-12-16 14:07:13 +0100

    now we can watch videos

---
+++ create_video_from_run.sh
@@ -0,0 +1,3 @@
+fps=10
+ffmpeg -r $fps -f image2 -s 960x720 -pattern_type glob -i 'run_details/iter_mask_*.png' -vcodec libx264 -crf 25  -pix_fmt yuv420p videos/masks.mp4
+ffmpeg -r $fps -f image2 -s 960x720 -pattern_type glob -i 'run_details/iter_weight_*.png' -vcodec libx264 -crf 25  -pix_fmt yuv420p videos/weight.mp4
