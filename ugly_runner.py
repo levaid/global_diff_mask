42afbc417f394def6a875e269a5aa67fc79b8a99
levaid <levai753@gmail.com>
2021-01-06 16:16:44 +0100

    runner fixup

--- ugly_runner.py
+++ ugly_runner.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import ParameterGrid
 import random
 from multiprocessing.dummy import Pool
 
-params_1 = {'num_first_stage': [0, 1, 2, 5, 10, 20, 50], 'initial_mode': ['weight', 'mask'], 'num_second_stage': [50],
-            'discretize': [True], 'discretization_quantile': [0.1, 0.3, 0.5, 0.7, 0.9], 'discretization_method': ['from_weight'], 'sigmoid': [False], 'neptune': [True]}
+params_1 = {'num_first_stage': [0, 1, 2, 5, 10, 20, 50], 'initial_mode': ['weight'], 'num_second_stage': [50],
+            'discretize': [True], 'discretization_quantile': [0.1, 0.3, 0.5, 0.7, 0.9], 'discretization_method': ['from_weight'], 'sigmoid': [False],
+            'neptune': [True]}
 
-params_2 = {'num_first_stage': [0, 1, 2, 5, 10, 20, 50], 'initial_mode': ['weight', 'mask'], 'num_second_stage': [50],
-            'discretize': [True], 'discretization_quantile': [0.1, 0.3, 0.5, 0.7, 0.9], 'discretization_method': ['from_mask'], 'sigmoid': [False, True], 'neptune': [True]}
+params_2 = {'num_first_stage': [0, 1, 2, 5, 10, 20, 50], 'initial_mode': ['mask'], 'num_second_stage': [50],
+            'discretize': [True, False], 'discretization_quantile': [0.1, 0.3, 0.5, 0.7, 0.9], 'discretization_method': ['from_mask'], 'sigmoid': [False, True],
+            'neptune': [True]}
 
-params_3 = {'num_first_stage': [0, 1, 2, 5, 10, 20, 50], 'initial_mode': ['weight', 'mask'], 'num_second_stage': [50],
-            'discretize': [False], 'sigmoid': [False, True], 'neptune': [True]}
 
 param_grid = list(ParameterGrid(params_1)) + list(ParameterGrid(params_2))
 random.shuffle(param_grid)
@@ -21,7 +21,7 @@ def run_with_args(cmdline_args):
     subprocess.run(['python', 'main.py'] + cmdline_args)
 
 
-p = Pool(4)
+p = Pool(3)
 
 for output, error in p.imap(run_with_args, cmdline_form_arguments):
     pass
